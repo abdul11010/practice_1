0f12ee51e999576f3bf01b57a9f693947713ac1d
abdul11010 <106474123+abdul11010@users.noreply.github.com>
2022-05-31 18:58:13 +0600

    Revert "Day3 completed"

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:practice_1/pages/home_page.dart';
 import 'package:practice_1/pages/login_page.dart';
-import 'package:google_fonts/google_fonts.dart';
 
-void main() {
+void main(){
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key key}) : super(key: key);
+  const MyApp({ Key key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       themeMode: ThemeMode.light,
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-        fontFamily: GoogleFonts.lato().fontFamily,
-      ),
+      theme: ThemeData(primarySwatch: Colors.deepPurple,),
       darkTheme: ThemeData(
         brightness: Brightness.light,
       ),
